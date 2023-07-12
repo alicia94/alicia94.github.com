e7ff96ac309a5898ab716cde6e1b17d212868081
alicia94 <zdxbupt0509@163.com>
2023-07-13 01:03:10 +0800

    Site updated: 2023-07-13 01:03:09

--- js/pagetitle.js
+++ js/pagetitle.js
@@ -2,14 +2,14 @@ var OriginTitle = document.title;
 var titleTime; 
 document.addEventListener('visibilitychange', function () { 
 	if (document.hidden) { 
-		$('[rel="icon"]').attr('href', "/funny.ico"); 
+		$('[rel="icon"]').attr('href', "/img/favicon.ico"); 
 		document.title = 'Σ(っ °Д °;)っ'; 
 		clearTimeout(titleTime); 
 		} 
 	else { 
-		$('[rel="icon"]').attr('href', "/favicon.ico"); 
+		$('[rel="icon"]').attr('href', "/img/favicon.ico"); 
 		document.title = '( ๑ ´ㅂ` ๑ )'; 
-		titleTime = setTimeout(function () { document.title = OriginTitle; }, 5000);
+		titleTime = setTimeout(function () { document.title = OriginTitle; }, 3000);
 		} 
 	}
 );
